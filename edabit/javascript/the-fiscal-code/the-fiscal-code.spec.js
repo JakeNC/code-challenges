ea233a707bdb1c6fd38769fc99d75363fb04fee8
Jake Spillane <jakespillane@gmail.com>
2022-11-18 16:37:19 -0500

    edabit - the fiscal code
    - add v1 solution
    - add instructions
    - copy test

---
+++ edabit/javascript/the-fiscal-code/the-fiscal-code.spec.js
@@ -0,0 +1,5 @@
+Test.assertEquals(fiscalCode({ name: "Brendan", surname: "Eich", gender: "M", dob: "1/12/1961" }), "CHEBND61T01")
+Test.assertEquals(fiscalCode({ name: "Helen", surname: "Yu", gender: "F", dob: "1/12/1950" }), "YUXHLN50T41")
+Test.assertEquals(fiscalCode({ name: "Al", surname: "Capone", gender: "M", dob: "17/1/1899" }), "CPNLAX99A17")
+Test.assertEquals(fiscalCode({ name: "Mickey", surname: "Mouse", gender: "M", dob: "16/1/1928" }), "MSOMKY28A16")
+Test.assertEquals(fiscalCode({ name: "Marie", surname: "Curie", gender: "F", dob: "7/11/1867" }), "CRUMRA67S47")
